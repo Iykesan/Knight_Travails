14aa13b9685236d7c75d876691d20cdbaa1b0fa1
Iyke <204012532+Iykesan@users.noreply.github.com>
2026-04-22 23:29:34 +0900

    initial commit

---
+++ knight.js
@@ -0,0 +1,96 @@
+const Node = (x, y) => {
+  return {
+    x,
+    y,
+    visited: false,
+    prev: null,
+  };
+};
+
+const Board = function () {
+  let board = [[], [], [], [], [], [], [], []];
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      board[i][j] = Node(i , j);
+    }
+  }
+  return {
+    knightMoves: function (start, end) {
+      this.search(start, end);
+      return this.path(start, end);
+    },
+
+    search: function (start, end) {
+      let queue = [];
+      queue.push(board[start[0]][start[1]]);
+      board[start[0]][start[1]].visited = true;
+
+      do{
+        let node = queue.shift();
+        if(node.x === end[0] && node.y === end[1]) break;
+        let possibleMoves = this.move(node);
+        // console.log("possible moves are: " + possibleMoves)
+        for(let i = 0; i < possibleMoves.length; i++){
+            let currentMove = possibleMoves[i];
+            if(board[currentMove[0]][currentMove[1]].visited === false){
+                queue.push(board[currentMove[0]][currentMove[1]])
+                board[currentMove[0]][currentMove[1]].visited = true;
+                board[currentMove[0]][currentMove[1]].prev = node;
+            }
+        }
+      }while(queue.length !== 0)
+    },
+
+    move: function(node){
+        let nextNodes = [];
+        const moves = [
+        [2, 1],
+        [2, -1],
+        [-2, 1],
+        [-2, -1],
+        [1, 2],
+        [-1, 2],
+        [1, -2],
+        [-1, -2],
+      ];
+      for(let i = 0; i < moves.length; i++){
+        if(((node.x + moves[i][0] >= 0) && (node.x + moves[i][0] < 8)) && ((node.y + moves[i][1] >= 0) && (node.y + moves[i][1] < 8))){
+            let arr = [node.x + moves[i][0], node.y + moves[i][1]];
+            nextNodes.push(arr)
+        }
+      }
+      return nextNodes;
+    },
+
+    path: function (start, end) {
+        let pathArray = []
+        let current = end;
+        pathArray.push(current)
+        do{
+            let prevX = board[current[0]][current[1]].prev.x
+            let prevY = board[current[0]][current[1]].prev.y
+            let previousNode = [prevX, prevY]
+            pathArray.push(previousNode);
+            current = previousNode;
+        }while(board[current[0]][current[1]].prev !== null)
+            pathArray.reverse();
+
+        if (pathArray[0][0] === start[0] && pathArray[0][1] === start[1])return pathArray;
+        return[];
+    },
+
+    showBoard: ()=>{
+        for(let i = 0; i < 8; i++){
+            for(let j = 0; j < 8; j++){
+                if(i === 0 && j === 1) continue;
+                console.log(`${board[i][j].x} ${board[i][j].y} ${board[i][j].prev.x} ${board[i][j].prev.y}`)
+            }
+        }
+    }
+  };
+};
+
+const board1 = Board();
+console.log(board1.knightMoves([0, 1], [3, 5]));
+
+
